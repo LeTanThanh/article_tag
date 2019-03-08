bc44534973c29e81fce8c4f10e329a47ae23eded
LeTanThanh <tanthanhcr95@gmail.com>
2019-03-08 14:02:47 +0700

    add_articles#show

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -2,4 +2,9 @@ class ArticlesController < ApplicationController
   def index
     @articles = Article.all
   end
+
+  def show
+    @article = Article.find_by id: params[:id]
+    @tags = @article.tags
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   root "articles#index"
 
-  resources :articles, only: %i|index|
+  resources :articles, only: %i|index show|
 end
